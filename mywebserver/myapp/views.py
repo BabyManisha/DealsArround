c05ac6e1635c6c35a13d4defd6dd3a2677c918d4
BabyManisha <babymaneesha@gmail.com>
2016-05-06 13:43:14 +0530

    mywenserver first sample commit

---
+++ mywebserver/myapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+	return HttpResponse("Hello, AmmaNaanna and Dear God PLease Bless Me.. :) :) Thank you for Every Thing!!..Love you....");
---
+++ mywebserver/mywebserver/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mywebserver.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', admin.site.urls),
+	url(r'^smdeals', 'myapp.views.index', name='index'),
+)
